7fa168035e6bb27013cc94229aaef761673edb2e
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-09-22 05:58:36 +1000

    Create lsb_pseudocode_v2.py

---
+++ src/lighting_scene_builder/lsb_pseudocode_v2.py
@@ -0,0 +1,36 @@
+This program will allow the user to import a model into Maya 
+and add lighting options to the model in order to create a render scene.
+
+Import Maya commands
+
+Create GUI window with buttons and logs
+
+Create import function
+	Import model from = 'location'
+	Confirm file location
+
+Create Buttons
+	Create Cube
+
+Create UI
+	Create window = window size
+	Column layout
+
+	Create Button 'Create'
+	Create Button 'Save'
+	Create Button 'Import'
+	Create Button 'Refresh'
+
+Enable light functions
+
+Enable shadows
+
+Set up directional light
+Set up intensity of light
+Set up intensity adjustment
+Set up shadow depth
+Add colour to lighting
+
+Rotate light at X-axis
+Rotate light at Y-axis
+Rotate light at Z-axis
