55779bfd3d57ee7ee535df575f7cecfc396a1265
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-09-22 04:13:20 +1000

    Update lsb_pseudocode.py

--- src/lighting_scene_builder/lsb_pseudocode.py
+++ src/lighting_scene_builder/lsb_pseudocode.py
@@ -1,7 +1,7 @@
 #This program will allow the user to import assets into Maya and add lighting options to the model
 #in order to create a render scene.
 
-import maya.cmds as mc #Import Maya Commands
+import maya.cmds as cmds
 
 def setImport(): #Create import function
     setImportPath = 'X:/Path_To_File.fbx' #Import model from = 'This Location'
@@ -9,16 +9,16 @@ def setImport(): #Create import function
 
 setImport()
 
-modelEditor(getPanel(withFocus = True), edit = True, displayLights = 'all') #Create light functions
+modelEditor(getPanel(withFocus = True), edit = True, displayLights = 'all')
 
-modelEditor(getPanel(withFocus = True), edit = True, shadows = True) #Create shadows
+modelEditor(getPanel(withFocus = True), edit = True, shadows = True)
 
-shadingNode('directionalLight', asLight = True) #Create Directional Light
-directionalLight (n = 'Light1') #Set up intensity of the light
-setAttr('Light1.intensity', 4) #Set up Input for intensity adjustment
-setAttr('Light1.useDepthMapShadows', 1) #Make shadows more smooth
-setAttr('Light1.color', 0, 0.5, 0.5, type = 'double3') #Add colour to lighting
+shadingNode('directionalLight', asLight = True)
+directionalLight (n = 'Light1')
+setAttr('Light1.intensity', 4)
+setAttr('Light1.useDepthMapShadows', 1)
+setAttr('Light1.color', 0, 0.5, 0.5, type = 'double3')
 
-setAttr('Light1.rotateX', -90) #Rotate at X-axis
-setAttr('Light1.rotateY', -90) #Rotate at Y-axis
-setAttr('Light1.rotateZ', -90) #Rotate at Z-axis
+setAttr('Light1.rotateX', -90)
+setAttr('Light1.rotateY', -90)
+setAttr('Light1.rotateZ', -90)
