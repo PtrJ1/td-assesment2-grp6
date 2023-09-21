1812c5f45597fcaf19dc0fbe92ebb2652b594e9a
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-09-22 06:07:19 +1000

    Update lsb_pseudocode.py

--- src/lighting_scene_builder/lsb_pseudocode.py
+++ src/lighting_scene_builder/lsb_pseudocode.py
@@ -15,13 +15,16 @@ def buttonMethod(args):
     cmds.PolyCube()
 
 def showUI():
-    myWindow = cmds.window(title="Lighting Manager", widthHeight = (500, 500)
-    cmds.columnLayout()
+    window = cmds.window(title="Lighting Manager", widthHeight = (500, 500)
+    cmds.columnLayout(adjustableColumn=True)
     
+    cmds.rowlayout(adjustableColumn3, numberOfColumns=4)
     cmds.button(label="Create"' command = buttonMethod)
     cmds.button(label="Save", command = buttonMethod)
     cmds.button(label="Import", command = buttonMethod)
     cmds.button(label="Refresh", command = buttonMethod)
+    
+cmds.showWindow(window)
 
 modelEditor(getPanel(withFocus = True), edit = True, displayLights = 'all')
 
@@ -36,3 +39,4 @@ setAttr('Light1.color', 0, 0.5, 0.5, type = 'double3')
 setAttr('Light1.rotateX', -90)
 setAttr('Light1.rotateY', -90)
 setAttr('Light1.rotateZ', -90)
+                
