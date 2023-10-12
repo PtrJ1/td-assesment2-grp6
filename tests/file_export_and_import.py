c61ab9b4ec48bb311ce6cad616cf6a726a9c24e6
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-10-13 01:08:39 +1100

    Update and rename file-export.py to file_export_and_import.py

--- tests/file_export_and_import.py
+++ tests/file_export_and_import.py
@@ -2,4 +2,8 @@ import maya.cmds as cmds
 
 path="C:/Example/"
 
+#Export File
 cmds.file(path + "new-lighting", force=True, typ="mayaBinary", exportSelected=True)
+
+#Import File
+cmds.file("assets/Lighting/new-lighting.ma", i=True)
