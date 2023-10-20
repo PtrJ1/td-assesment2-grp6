be5b1d39ffb713f8d37ef4edd1d3914cacbe3a8b
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-10-20 12:03:54 +1100

    Update lighting_tool_13604677.py

--- tests/lighting_tool_13604677.py
+++ tests/lighting_tool_13604677.py
@@ -1,5 +1,29 @@
 import maya.cmds as cmds
 
-path="C:/Example/"
+path="C:/Users/13604677/Desktop/Lighting-File/"
 
 cmds.file(path + "new-lighting", force=True, typ="mayaBinary", exportSelected=True)
+
+class Window(object):
+        
+    def __init__(self):
+            
+        self.window = "Window"
+        self.title = "Light Exporter"
+        self.size = (200, 100)
+            
+        if cmds.window(self.window, exists = True):
+            cmds.deleteUI(self.window, window=True)
+            
+        self.window = cmds.window(self.window, title=self.title, widthHeight=self.size)
+        
+        cmds.columnLayout(adjustableColumn = True)
+
+        cmds.button(label='Import')
+        cmds.separator(h=10)
+        cmds.button(label='Export')
+        
+        cmds.showWindow()
+
+                                   
+myWindow = Window()
