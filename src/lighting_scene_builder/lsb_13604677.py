a1c2ce229f082481c424074045a0794b825e94d6
EngineerMain261 <82277666+EngineerMain261@users.noreply.github.com>
2023-11-02 05:43:16 +1100

    Update lsb_13604677.py
    Updated the UI, added combo box and more buttons

--- src/lighting_scene_builder/lsb_13604677.py
+++ src/lighting_scene_builder/lsb_13604677.py
@@ -4,14 +4,55 @@ def create_scene_setup_ui():
     if cmds.window("Setup Window", exists=True):
         cmds.deleteUI("Setup Window")
     
-    scene_setup_win = cmds.window("Setup Window", title="Scene Setup Tool", widthHeight=(300, 200))
+    scene_setup_win = cmds.window("Setup Window", title="Lighting Scene Tool", widthHeight=(500, 400))
     cmds.columnLayout(adjustableColumn=True)
 
     # Create a text field for file path
     file_path_field = cmds.textFieldGrp(label="FBX File Path: ", columnWidth=[1, 100])
     
+    cmds.separator(h=10)
+    
     # Create a button to browse and insert the FBX file
     cmds.button(label="Browse and Insert FBX", command=lambda *args: insert_fbx(file_path_field))
+    
+    cmds.separator(h=10)
+    
+    # Create the first combo box
+    character_combo = cmds.optionMenu(label="Character:")
+    # Add items to the combo box as needed
+    cmds.menuItem(label="Character 1")
+    cmds.menuItem(label="Character 2")
+    
+    cmds.separator(h=10)
+
+    # Create the second combo box
+    prop_combo = cmds.optionMenu(label="Prop:")
+    # Add items to the combo box as needed
+    cmds.menuItem(label="Prop 1")
+    cmds.menuItem(label="Prop 2")
+    
+    cmds.separator(h=10)
+    
+    # Create 'Import Character' button
+    cmds.button(label="Import Character", command=lambda *args: import_selected("Character"))
+    
+    cmds.separator(h=10)
+
+    # Create 'Import Prop' button
+    cmds.button(label="Import Prop", command=lambda *args: import_selected("Prop"))
+    
+    cmds.separator(h=10)
+
+    # Create 'Import Camera' button
+    cmds.button(label="Import Camera", command=lambda *args: import_selected("Camera"))
+    
+    cmds.separator(h=10)
+    
+    cmds.button(label="Check Version")
+    
+    cmds.separator(h=10)
+    
+    cmds.button(label="Update Version")
     
     cmds.showWindow(scene_setup_win)
 
